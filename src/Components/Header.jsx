6b653f68fb4ac921681692c9d9f6a8b18b05445b
NguyenQui9112003 <147489314+NguyenQui9112003@users.noreply.github.com>
2024-10-28 17:59:40 +0700

    test

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <>
-            <p className="text-2xl italic font-bold text-blue-600 m-3">Unflash Gallery</p>
+            <p className="text-2xl italic font-bold text-blue-600 m-3">Unflash Gallery test</p>
         </>
     )
 }
